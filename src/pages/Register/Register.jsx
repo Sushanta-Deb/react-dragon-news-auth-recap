c7812af37fabfc300cc6fa18a178c0490331ceef
Sushanta deb <himelsom@gmail.com>
2024-01-01 21:33:08 +0600

    Login and Register form done from using daisy ui

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,23 +2,35 @@ import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
-const Login = () => {
-    const handleLogin = e => {
+const Register = () => {
+    const handleRegister = e => {
         e.preventDefault();
         const form = new FormData(e.currentTarget);
-        console.log(form.get('password'))
+        console.log(form)
     }
     return (
         <div>
             <Navbar></Navbar>,
             <h2 className="text-2xl text-center mt-3 mb-3">Please Login</h2>
             <div className="mx-auto card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleLogin} className="card-body">
+                        <form onSubmit={handleRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -30,13 +42,13 @@ const Login = () => {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn btn-secondary">Register</button>
                             </div>
                         </form>
                     </div>
-                    <p className="text-center mt-3">Do not have an account? <Link className="text-blue-600 font-semibold" to='/register'>Register</Link></p>
+                    <p className="text-center mt-3">Already have an account? <Link className="text-blue-600 font-semibold" to='/login'>Login</Link></p>
         </div>
     );
 };
 
-export default Login;
+export default Register;
